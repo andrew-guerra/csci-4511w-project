db50bb697ef7234032598e98628d6fde90626600
guerr242 <guerr242@umn.edu>
2023-05-02 10:35:45 -0500

    initial commit

---
+++ libs/UNO.py
@@ -0,0 +1,41 @@
+import random
+from aima-python.games import Game
+
+class UNO(Game):
+    def actions(self, state):
+        """Return a list of the allowable moves at this point."""
+        raise NotImplementedError
+
+    def result(self, state, move):
+        """Return the state that results from making a move from a state."""
+        raise NotImplementedError
+
+    def utility(self, state, player):
+        """Return the value of this final state to player."""
+        raise NotImplementedError
+
+    def terminal_test(self, state):
+        """Return True if this is a final state for the game."""
+        return not self.actions(state)
+
+    def to_move(self, state):
+        """Return the player whose move it is in this state."""
+        return state.to_move
+
+    def display(self, state):
+        """Print or otherwise display the state."""
+        print(state)
+
+    def __repr__(self):
+        return '<{}>'.format(self.__class__.__name__)
+
+    def play_game(self, *players):
+        """Play an n-person, move-alternating game."""
+        state = self.initial
+        while True:
+            for player in players:
+                move = player(self, state)
+                state = self.result(state, move)
+                if self.terminal_test(state):
+                    self.display(state)
+                    return self.utility(state, self.to_move(self.initial))
